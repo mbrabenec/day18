106070c144d28be9553ce1b821524d1918709e6e
mbrabenec <brabzzz@gmail.com>
2021-01-28 14:50:07 +0100

    ul

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,19 @@
 class ProgressBar {
-
-    constructor(min, max, step) {
-
-        this.level = min;
-        this.min = min;
-        this.max = max;
-        this.step = step;
-        
-        this.bar = this._createBar()
-
-    }
-
-    _createBar() {
-
-        const div = document.createElement("div");
-        div.innerHTML = 
-        `
-        <div class="label">
+  constructor(from, to, step) {
+    this.current = from
+    this.from = from
+    this.to = to
+    this.step = step
+    
+    this.bar = this._createBar()
+  }
+  
+  _createBar() {
+    const div = document.createElement('div')
+    div.innerHTML = `
+      <div class="label">
         <span class="counter"></span>/<span class="maximum"></span>
       </div>
-  
       <div class="progress">
         <div class="btn-minus"></div>
         <div class="bar">
@@ -27,55 +21,45 @@ class ProgressBar {
         </div>
         <div class="btn-plus"></div>
       </div>
-        `
-        div.querySelector(".counter").textContent = this.min;
-        div.querySelector(".maximum").textContent = this.max;
-        div.querySelector(".knob").style.width = `${(this.min / this.max) * 100}%`
-
-        div.querySelector(".btn-minus").addEventListener("click", () => {this.minus()});
-        div.querySelector(".btn-plus").addEventListener("click", this.plus);
-
-        return div;
-    }
-
-
-
-    updateAll () {
-        this.bar.querySelector(".knob").style.width = `${(this.current / this.to) * 100}%`
-        this.bar.querySelector(".counter").textContent = this.level; 
-    }
-
-
-    minus () {
-        if (this.level - this.step >= this.min) {
-          this.level-= this.step;
-          this.updateAll()
-        }
-        
-      }
+    `
     
-    plus () {
-        if (this.level + this.step <= this.max) {
-            this.level+= this.step;
-            this.updateAll()    
-        }
-        
-    }
-
-
+    div.querySelector('.counter').textContent = this.from
+    div.querySelector('.maximum').textContent = this.to
+    div.querySelector('.knob').style.width = `${(this.from / this.to) * 100}%`
+    div.querySelector('.btn-minus').addEventListener('click', () => {
+      this.decrease()
+    })
+    div.querySelector('.btn-plus').addEventListener('click', () => {
+      this.increase()
+    })
+    return div
+  }
   
-
-    renderTo (element) {
-        element.appendChild(this.bar);
+  decrease() {
+    if (this.current - this.step >= this.from) {
+      this.current -= this.step
     }
-
-
-
-
-
-
+    this.update()
+  }
+  
+  increase() {
+    if (this.current + this.step <= this.to) {
+      this.current += this.step
+    }
+    this.update()
+  }
+  
+  update() {
+    this.bar.querySelector('.knob').style.width = `${(this.current / this.to) * 100}%`
+    this.bar.querySelector('.counter').textContent = this.current
+  }
+  
+  renderTo(element) {
+    element.appendChild(this.bar)
+  }
 }
 
+const bar = new ProgressBar(0, 200, 50)
+bar.renderTo(document.body)
 
-const bar1 = new ProgressBar(2, 10, 1);
-bar1.renderTo(document.body);
+const bar2 = new ProgressBar(0, 50, 13)
